d319b7675b11f5c575efb18b7f0e0e4511ac5335
Soepski <ronlieb@hotmail.com>
2020-04-16 14:53:37 +0200

    Seriele communicatie
    Het verzenden en ontvangen van berichten werkt. Deze worden nu nog in een lijst weergegeven. Deze moeten nog in de historie komen.

--- StayFitApp/Form1.Designer.cs
+++ StayFitApp/Form1.Designer.cs
@@ -28,17 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.btnHistory = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.tmStopwatch = new System.Windows.Forms.Timer(this.components);
+            this.btStuur = new System.Windows.Forms.Button();
+            this.lbBerichten = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.btDisconnect = new System.Windows.Forms.Button();
+            this.cbOefeningen = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // btnHistory
             // 
-            this.btnHistory.Location = new System.Drawing.Point(12, 12);
+            this.btnHistory.Location = new System.Drawing.Point(161, 11);
+            this.btnHistory.Margin = new System.Windows.Forms.Padding(2);
             this.btnHistory.Name = "btnHistory";
-            this.btnHistory.Size = new System.Drawing.Size(95, 44);
+            this.btnHistory.Size = new System.Drawing.Size(71, 36);
             this.btnHistory.TabIndex = 3;
             this.btnHistory.Text = "History";
             this.btnHistory.UseVisualStyleBackColor = true;
@@ -46,34 +51,110 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(113, 12);
+            this.btnExit.Location = new System.Drawing.Point(236, 11);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(95, 44);
+            this.btnExit.Size = new System.Drawing.Size(71, 36);
             this.btnExit.TabIndex = 4;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // btStuur
+            // 
+            this.btStuur.Location = new System.Drawing.Point(190, 65);
+            this.btStuur.Name = "btStuur";
+            this.btStuur.Size = new System.Drawing.Size(75, 23);
+            this.btStuur.TabIndex = 6;
+            this.btStuur.Text = "Stuur bericht";
+            this.btStuur.UseVisualStyleBackColor = true;
+            this.btStuur.Click += new System.EventHandler(this.btStuur_Click);
+            // 
+            // lbBerichten
+            // 
+            this.lbBerichten.FormattingEnabled = true;
+            this.lbBerichten.Location = new System.Drawing.Point(13, 142);
+            this.lbBerichten.Name = "lbBerichten";
+            this.lbBerichten.Size = new System.Drawing.Size(252, 212);
+            this.lbBerichten.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Ontvangen berichten:";
+            // 
+            // btConnect
+            // 
+            this.btConnect.BackColor = System.Drawing.Color.Red;
+            this.btConnect.Location = new System.Drawing.Point(11, 11);
+            this.btConnect.Margin = new System.Windows.Forms.Padding(2);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(71, 36);
+            this.btConnect.TabIndex = 9;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = false;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // btDisconnect
+            // 
+            this.btDisconnect.Location = new System.Drawing.Point(86, 11);
+            this.btDisconnect.Margin = new System.Windows.Forms.Padding(2);
+            this.btDisconnect.Name = "btDisconnect";
+            this.btDisconnect.Size = new System.Drawing.Size(71, 36);
+            this.btDisconnect.TabIndex = 10;
+            this.btDisconnect.Text = "Disconnect";
+            this.btDisconnect.UseVisualStyleBackColor = true;
+            this.btDisconnect.Click += new System.EventHandler(this.btDisconnect_Click);
+            // 
+            // cbOefeningen
+            // 
+            this.cbOefeningen.FormattingEnabled = true;
+            this.cbOefeningen.Items.AddRange(new object[] {
+            "10 Sit-ups",
+            "20 Burpees",
+            "5 Radslagen "});
+            this.cbOefeningen.Location = new System.Drawing.Point(15, 65);
+            this.cbOefeningen.Name = "cbOefeningen";
+            this.cbOefeningen.Size = new System.Drawing.Size(121, 21);
+            this.cbOefeningen.TabIndex = 11;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.ControlBox = false;
+            this.Controls.Add(this.cbOefeningen);
+            this.Controls.Add(this.btDisconnect);
+            this.Controls.Add(this.btConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbBerichten);
+            this.Controls.Add(this.btStuur);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnHistory);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button btnHistory;
         private System.Windows.Forms.Button btnExit;
-        private System.Windows.Forms.Timer tmStopwatch;
+        private System.Windows.Forms.Button btStuur;
+        private System.Windows.Forms.ListBox lbBerichten;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.Button btDisconnect;
+        private System.Windows.Forms.ComboBox cbOefeningen;
     }
 }
 
--- StayFitApp/Form1.cs
+++ StayFitApp/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using StayFitApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +12,105 @@ using System.Windows.Forms;
 
 namespace StayFitApp
 {
+
     public partial class Form1 : Form
     {
+        SerialMessenger serialMessenger;
+        MessageBuilder messageBuilder;
+        string PortName = "COM3";
+        int BaudRate = 9600;
+
+        Timer readMessageTimer = new Timer();
+
         public int time;
+        
         public Form1()
         {
             InitializeComponent();
+
+            messageBuilder = new MessageBuilder();
+            serialMessenger = new SerialMessenger(PortName, BaudRate, messageBuilder);
+
+            readMessageTimer.Interval = 10;
+            readMessageTimer.Tick += new EventHandler(ReadMessageTimer_Tick);
+
         }
+
+        private void ReadMessageTimer_Tick(object sender, EventArgs e)
+        {
+            string message = serialMessenger.ReadMessages();
+            if (message != null && message != "")
+            {
+                processReceivedMessage(message);
+            }
+        }
+
+        private void processReceivedMessage(string message)
+        {
+            // First trim whitespace characters like a trailing '\r'.
+            // This is needed because the Arduino Serial.println adds \r\n.
+            // '\n' will be removed because this is used as the message separation character,
+            // but the '\r' must also be removed, otherwise comparing the message strings will not work.
+            message = message.Trim();
+            // Add message to the listBox.
+            lbBerichten.Items.Add(message);
+            // TODO: Below fill in your message handling.
+
+        }
+
         private void btnHistory_Click(object sender, EventArgs e)
         {
             this.Hide();
-            var newForm = new HistoryView();
-            newForm.Show();
+            HistoryView historyview = new HistoryView();
+            historyview.Show();
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
             Environment.Exit(1);
+        }
+
+        private void btStuur_Click(object sender, EventArgs e)
+        {
+            string message = $"#exercise:<{cbOefeningen.Text}>%";
+            MessageBox.Show(message);
+            serialMessenger.SendMessage(message);
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialMessenger.Connect();
+                readMessageTimer.Enabled = true;
+                Console.WriteLine("Connected to Arduino" + PortName);
+                btConnect.BackColor = Color.Green;
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void btDisconnect_Click(object sender, EventArgs e)
+        {
+            disconnect();
+            Console.WriteLine("not connected");
+            btConnect.BackColor = Color.Red;
+        }
+
+        private void disconnect()
+        {
+            try
+            {
+                readMessageTimer.Enabled = false;
+                serialMessenger.Disconnect();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
         }
     }
 }
